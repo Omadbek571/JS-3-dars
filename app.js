64d5af3d944f73343664beb38fa921e5f5e41bcc
Omadbek571 <omadbektomsk08@gmail.com>
2024-03-01 20:13:20 +0500

    JS-3-VAZIFAM

---
+++ app.js
@@ -0,0 +1,26 @@
+// Funksiyani yaratamiz
+function hisoblash(a, b, operator) {
+    let natija;
+    // Operatorni tekshiramiz va mos funksiyani ishga tushiramiz
+    if (operator === '+') {
+        natija = a + b;
+    } else if (operator === '-') {
+        natija = a - b;
+    } else if (operator === '*') {
+        natija = a * b;
+    } else if (operator === '/') {
+        natija = a / b;
+    } else {
+        // Noto'g'ri operator uchun xatolik xabarini chiqaramiz
+        natija = 'Notogri operator';
+    }
+    // Natijani qaytarib beramiz
+    return natija;
+}
+
+// Test qilib ko'ramiz
+let son1 = (+prompt("BIRINCHI SONI KIRITING"));
+let operator = (prompt("OPERATOR KIRITING YANI (+ - / *)"));
+let son2 = (+prompt("IKINCHI SONI KIRITING"));
+let netija = hisoblash(son1, son2, operator);
+console.log(`${son1} ${operator} ${son2} = ${netija}`);
